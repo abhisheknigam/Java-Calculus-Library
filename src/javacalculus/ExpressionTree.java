8c1247a4803265e809e2fa934f4d9fc0a4ef7645
sshannin <sshannin@bbaacb8a-c8d7-11dd-8772-d1b041bf0e37>
2008-12-15 09:42:48 +0000

    ExpressionTree now works for all straight numbers and operators; functions should be a simple addition

--- src/javacalculus/ExpressionTree.java
+++ src/javacalculus/ExpressionTree.java
@@ -7,16 +7,14 @@ public class ExpressionTree
 	private Node head;
 	
 	public ExpressionTree (String expression)
-	{
-		makeMyTree(expression);
-	}
+	{	head=makeTree(expression);	}
 	
 	public ExpressionTree (ArrayList<String> expression)
 	{	
 		StringBuffer buffer = new StringBuffer();
 		for (String element: expression)
 			buffer.append(element);
-		makeMyTree(buffer.toString());
+		head=makeTree(buffer.toString());
 	}
 	
 	public ExpressionTree (String[] expression)
@@ -24,68 +22,221 @@ public class ExpressionTree
 		StringBuffer buffer = new StringBuffer();
 		for (String element: expression)
 			buffer.append(element);
-		makeMyTree(buffer.toString());
+		head=makeTree(buffer.toString());
 	}
 	
-	private Node makeMyTree(String expression)
-	{
 	
-		Node current=head;
+	private int getLevelOneIndex(String expression)
+	{
+		System.out.println("+/- dealer received: "+expression);
+		
+		int parCounter=0;
 		for (int i=0; i<expression.length(); i++)
 		{
-			char a=expression.charAt(i);
-			char prev=expression.charAt(i-1);
-			//Farms out parentheses recursively
-			if(a=='(')
+			char cur=expression.charAt(i);
+			if(parCounter==0)
 			{
-				int leftCounter=0;
-				for (int j=i+1;j<expression.length();j++)
-				{
-					char b=expression.charAt(j);
-					if(b=='(')
-						leftCounter++;
-					else if (b==')')
-					{
-						if (leftCounter==0)
-						{
-							current=makeMyTree(expression.substring(i+1,j-1));
-							break;
-						}
-						else
-							leftCounter--;
-					}
-				}
+				if (cur=='+'||cur=='-'||cur=='%')
+					return i;
 			}
-			//makes the bottom double
-			if(Character.isDigit(a)||a=='.')
+			if(cur=='(')
+					parCounter++;	
+			if (cur==')')
+				parCounter--;
+		}		
+	
+		return -1;
+	}
+	
+	private int getLevelTwoIndex(String expression)
+	{
+//System.out.println("*// dealer received: "+expression);
+		int parCounter=0;
+		for (int i=0; i<expression.length(); i++)
+		{
+			char cur=expression.charAt(i);
+			if(parCounter==0)
 			{
-				StringBuffer number=new StringBuffer();
-				number.append(a);
-				for (int j=i+1;j<expression.length();j++)
+				if (cur=='/'||cur=='*')
+					return i;
+			}
+			if(cur=='(')
+					parCounter++;	
+			if (cur==')')
+				parCounter--;
+				
+		}
+		
+		return -1;
+	}
+	
+	private int getLevelThreeIndex(String expression)
+	{
+//System.out.println("^ dealer received: "+expression);
+		
+		int parCounter=0;
+		for (int i=0; i<expression.length(); i++)
+		{
+			char cur=expression.charAt(i);
+			if(parCounter==0)
+			{
+				if (cur=='^')
+					return i;
+			}
+			if(cur=='(')
+					parCounter++;	
+			if (cur==')')
+				parCounter--;
+				
+		}		
+		
+		return -1;
+	}
+	
+	private int getLevelFourIndex(String expression)
+	{
+		
+//		for (int i=0; i<expression.length(); i++)
+//		{
+//			char cur=expression.charAt(i);
+//			int parCounter=0;
+//			while(parCounter==0)
+//			{
+//				if (cur=='^')
+//					return i;
+//			}
+//			if(cur=='(')
+//					parCounter++;	
+//			if (cur==')')
+//				parCounter--;
+//				
+//		}		
+		return -1;
+	}
+	
+	private boolean stripPars(String expression)
+	{
+		if(expression.charAt(0)!='(')
+			return false;
+		if(expression.charAt(expression.length()-1)!=')')
+			return false;
+		int parCount=0;
+		for (int i=1;i<expression.length()-1;i++)
+		{
+			if (expression.charAt(i)=='(')
+				parCount++;
+			if(expression.charAt(i)==')')
+				parCount--;
+			if(parCount<0)
+				return false;
+		}
+		return true;
+	}
+	
+	private Node makeTree(String expression)
+	{
+		System.out.println("String received: "+expression);
+		Node myTop=null;
+		int headIndex=getLevelOneIndex(expression);
+		System.out.println("Index returned"+headIndex);
+		if(stripPars(expression))
+			return makeTree(expression.substring(1,expression.length()-1));
+		if(headIndex!=-1)
+			myTop=new Node(""+expression.charAt(headIndex),makeTree(expression.substring(0,headIndex)),
+				makeTree(expression.substring(headIndex+1,expression.length())));
+		else
+		{
+			headIndex=getLevelTwoIndex(expression);
+			if(headIndex!=-1)
+				myTop=new Node(""+expression.charAt(headIndex),makeTree(expression.substring(0,headIndex)),
+					makeTree(expression.substring(headIndex+1,expression.length())));
+			else
+			{
+				headIndex=getLevelThreeIndex(expression);
+				if(headIndex!=-1)
+					myTop=new Node(""+expression.charAt(headIndex),makeTree(expression.substring(0,headIndex)),
+						makeTree(expression.substring(headIndex+1,expression.length())));
+				else
 				{
-					char b=expression.charAt(j);
-					if(Character.isDigit(b)||b=='.')
-						number.append(b);
+					headIndex=getLevelFourIndex(expression);
+					if(headIndex!=-1)
+						myTop=new Node(""+expression.charAt(headIndex),makeTree(expression.substring(0,headIndex)),
+							makeTree(expression.substring(headIndex+1,expression.length())));
 					else
 					{
-						current=new Node(number.toString(),current,null,null);
-						break;
+						System.out.println("Assigning this expression: "+expression);
+						myTop=new Node(expression,null,null);
 					}
+						
 				}
 			}
-			
-			
-			if (a=='+'||a=='*'||a=='/'||a=='%'||(a=='-'&&(prev=='+'||prev=='*'||prev=='/'||prev=='%')))
-			{
-				current=new Node(""+a,current,makeMyTree(expression.substring(0,i-1)),
-						makeMyTree(expression.substring(i+1,expression.length()-1)));
-			}
-			
-			
 		}
-		return null;
 		
+		return myTop;
 	}
+	
+	public double eval()
+	{	return head.eval();	}
+	
+//	private Node makeMyTree(String expression)
+//	{
+//	
+//		Node current=head;
+//		//this loop breaks expression subterms
+//		for (int i=0; i<expression.length(); i++)
+//		{
+//			char a=expression.charAt(i);
+//			char prev=expression.charAt(i-1);
+//			
+//			if (a=='+'||a=='*'||a=='/'||a=='%'||(a=='-'&&(prev=='+'||prev=='*'||prev=='/'||prev=='%')))
+//			{
+//				current=new Node(""+a,current,makeMyTree(expression.substring(0,i-1)),
+//						makeMyTree(expression.substring(i+1,expression.length()-1)));
+//			}
+//			//Farms out parentheses recursively
+//			if(a=='(')
+//			{
+//				int leftCounter=0;
+//				for (int j=i+1;j<expression.length();j++)
+//				{
+//					char b=expression.charAt(j);
+//					if(b=='(')
+//						leftCounter++;
+//					else if (b==')')
+//					{
+//						if (leftCounter==0)
+//						{
+//							current=makeMyTree(expression.substring(i+1,j-1));
+//							break;
+//						}
+//						else
+//							leftCounter--;
+//					}
+//				}
+//			}
+//			//makes the bottom double
+//			if(Character.isDigit(a)||a=='.')
+//			{
+//				StringBuffer number=new StringBuffer();
+//				number.append(a);
+//				for (int j=i+1;j<expression.length();j++)
+//				{
+//					char b=expression.charAt(j);
+//					if(Character.isDigit(b)||b=='.')
+//						number.append(b);
+//					else
+//					{
+//						current=new Node(number.toString(),current,null,null);
+//						break;
+//					}
+//				}
+//			}
+//						
+//			
+//		}
+//		return null;
+//		
+//	}
 	
 
 private class Node
@@ -93,14 +244,12 @@ private class Node
 	private String myValue;
 	private Node left;
 	private Node right;
-	private Node parent;
 	
-	public Node (String value, Node parent, Node left, Node right)
+	public Node (String value, Node left, Node right)
 	{
 		myValue=value;
 		this.left=left;
 		this.right=right;
-		this.parent=parent;
 	}
 	
 	public Node getLeft()
@@ -108,13 +257,7 @@ private class Node
 	
 	public Node getRight()
 	{	return right;	}
-	
-	public Node getParent()
-	{	return parent;	}
-	
-	public void setParent(Node p)
-	{	parent=p;	}
-	
+
 	public void setLeft(Node l)
 	{	left=l;	}
 	
--- src/test/TestAPI.java
+++ src/test/TestAPI.java
@@ -3,7 +3,8 @@ package test;
 import javacalculus.*;
 
 public class TestAPI {
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		CalcExpression exp = new CalcExpression("1+sIn(pi-e*78/6+9.81) %e*tan(14.5)-abs(-1)");
 		System.out.println(exp.value());
 	}
