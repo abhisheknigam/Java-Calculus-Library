19a78593a7a84470d97d1c7afa24f8d714d926f0
sshannin <sshannin@bbaacb8a-c8d7-11dd-8772-d1b041bf0e37>
2008-12-15 06:24:41 +0000

    Added to expressiontree; now just have to add initial parser to break the string into a tree.

--- src/javacalculus/ExpressionTree.java
+++ src/javacalculus/ExpressionTree.java
@@ -1,13 +1,45 @@
 package javacalculus;
 
+import java.util.ArrayList;
+
 public class ExpressionTree 
 {
 	private Node Head;
-	private String myExpression;
-
+	private String myExpression=null;
+	
 	public ExpressionTree (String expression)
 	{
-		myExpression=expression;		
+		makeMyTree(expression);
+	}
+	
+	public ExpressionTree (ArrayList<String> expression)
+	{	
+		StringBuffer buffer = new StringBuffer();
+		for (String element: expression)
+			buffer.append(element);
+		makeMyArray(buffer.toString());
+	}
+	
+	public ExpressionTree (String[] expression)
+	{	
+		StringBuffer buffer = new StringBuffer();
+		for (String element: expression)
+			buffer.append(element);
+		makeMyArray(buffer.toString());
+	}
+	
+	private void makeMyArray(String expression)
+	{
+		
+	}
+	
+	private void makeMyTree(String expression)
+	{
+		for (int i=0; i<expression.length(); i++)
+		{
+			
+		}
+		
 	}
 
 	private boolean addNode(Node toAdd)
@@ -32,26 +64,73 @@ private class Node
 	
 	public double eval()
 	{
-		if (left==null&&right==null)
-			return Double.parseDouble(myValue);
-		else
+		if (left==null&&right==null) //if a constant
 		{
+			if (myValue.equalsIgnoreCase("PI"))
+				return Math.PI;
+			else if (myValue.equalsIgnoreCase("e"))
+				return Math.E;
+			else
+				return Double.parseDouble(myValue);
+		}
+		
+		else if (left!=null&&right!=null) //binary operators
+			{
 			if (myValue.charAt(0)=='+')
 				return left.eval()+right.eval();
 			if (myValue.charAt(0)=='-')
-				if(right!=null)
 					return left.eval()-right.eval();
-				else
-					return -(left.eval());
 			if (myValue.charAt(0)=='*')
 				return left.eval()*right.eval();
 			if (myValue.charAt(0)=='/')
 				return left.eval()/right.eval();
 			if (myValue.charAt(0)=='^')
 				return Math.pow(left.eval(),right.eval());
+			if (myValue.charAt(0)=='%')
+				return left.eval()%right.eval();
+			}
+		else //unary operators
+		{
+			if (myValue.charAt(0)=='-')
+				return -(left.eval());
+			if (myValue.equalsIgnoreCase("abs"))
+				return (Math.abs(left.eval()));
+			if (myValue.equalsIgnoreCase("sqrt"))
+				return (Math.sqrt(left.eval()));
 			
-			
-			
+			//trigs, including inverses, hyperbolics
+			if (myValue.equalsIgnoreCase("tan"))
+				return (Math.tan(left.eval()));
+			if (myValue.equalsIgnoreCase("sin"))
+				return (Math.sin(left.eval()));
+			if (myValue.equalsIgnoreCase("cos"))
+				return (Math.cos(left.eval()));
+			if (myValue.equalsIgnoreCase("cot"))
+				return (1/Math.tan(left.eval()));
+			if (myValue.equalsIgnoreCase("sec"))
+				return (1/Math.cos(left.eval()));
+			if (myValue.equalsIgnoreCase("csc"))
+				return (1/Math.sin(left.eval()));
+			if (myValue.equalsIgnoreCase("arctan"))
+				return (Math.atan(left.eval()));
+			if (myValue.equalsIgnoreCase("atan"))
+				return (Math.atan(left.eval()));
+			if (myValue.equalsIgnoreCase("arcsin"))
+				return (Math.asin(left.eval()));
+			if (myValue.equalsIgnoreCase("asin"))
+				return (Math.asin(left.eval()));
+			if (myValue.equalsIgnoreCase("tan"))
+				return (Math.tan(left.eval()));
+			if (myValue.equalsIgnoreCase("arccos"))
+				return (Math.acos(left.eval()));
+			if (myValue.equalsIgnoreCase("acos"))
+				return (Math.acos(left.eval()));
+			if (myValue.equalsIgnoreCase("tanh"))
+				return (Math.tanh(left.eval()));
+			if (myValue.equalsIgnoreCase("cosh"))
+				return (Math.cosh(left.eval()));
+			if (myValue.equalsIgnoreCase("sinh"))
+				return (Math.sinh(left.eval()));
 		}
 		return 0;
 	}
