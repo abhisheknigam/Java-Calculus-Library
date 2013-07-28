f482d16b6fd158457717de52fb2fcd4dac6050e7
Seva.Luchianov <Seva.Luchianov@gmail.com>
2013-07-28 01:32:19 +0000

    New thread class for int by parts

--- src/javacalculus/evaluator/IntegrationThread.java
+++ src/javacalculus/evaluator/IntegrationThread.java
@@ -4,8 +4,10 @@
  */
 package javacalculus.evaluator;
 
+import java.util.ArrayList;
 import javacalculus.core.CALC;
-import javacalculus.struct.CalcInteger;
+import javacalculus.struct.CalcError;
+import javacalculus.struct.CalcFunction;
 import javacalculus.struct.CalcObject;
 import javacalculus.struct.CalcSymbol;
 
@@ -13,27 +15,40 @@ import javacalculus.struct.CalcSymbol;
  *
  * @author Seva
  */
-public class IntegrationThread extends Thread {
+public class IntegrationThread implements Runnable {
 
-    public CalcObject answer = null;
     private CalcObject[] udvPair;
     private CalcSymbol var;
-    private CalcINT overlord;
+    private CalcINTBYPARTS overlord;
 
-    public IntegrationThread(CalcObject[] pair, CalcSymbol var, CalcINT overlord) {
+    public IntegrationThread(CalcObject[] pair, CalcSymbol var, CalcINTBYPARTS overlord) {
         udvPair = pair;
         this.var = var;
         this.overlord = overlord;
+        //if (overlord.isMaster()) {
+        //    System.out.println("I have the honor of being a master thread " + toString());
+        //}
+    }
+
+    @Override
+    public String toString() {
+        return "I am " + Thread.currentThread().getName() + " and my overlord is the overlord master: " + (overlord.isMaster()) + "";
     }
 
     @Override
     public void run() {
-        while (overlord.intByPartsThreadsAreAlive) {
+        //toString();
+        if (overlord.keepGoing()) {
             try {
-                Thread.sleep(1);
+                //Thread.sleep(10);
                 CalcObject u = udvPair[0];
                 CalcObject dv = udvPair[1];
-                CalcObject v = CALC.SYM_EVAL(CALC.INT.createFunction(dv, var));
+                //System.out.println("I am " + Thread.currentThread().getName() + " and my overlord is the overlord master: " + overlord.overlord == null);
+                //Thread.sleep(10);
+                //System.out.println("hi");
+                CalcINT vIntegrator = new CalcINT();
+                vIntegrator.intByPartsCarryOver = overlord;
+                CalcObject v = CALC.SYM_EVAL(vIntegrator.integrate(dv, var));
                 u = CALC.SYM_EVAL(u);
                 v = CALC.SYM_EVAL(v);
                 //System.out.println("This is our u: " + u);
@@ -43,11 +58,24 @@ public class IntegrationThread extends Thread {
                 //System.out.println("This is our du: " + du);
                 CalcObject uTimesV = CALC.SYM_EVAL(CALC.MULTIPLY.createFunction(u, v));
                 CalcObject vTimesDu = CALC.SYM_EVAL(CALC.MULTIPLY.createFunction(v, du));
-                answer = CALC.SYM_EVAL(CALC.ADD.createFunction(uTimesV, CALC.MULTIPLY.createFunction(CALC.INT.createFunction(vTimesDu, var), CALC.NEG_ONE)));
+                CalcINT vduIntegrator = new CalcINT();
+                vduIntegrator.intByPartsCarryOver = overlord;
+                //System.out.println(toString());
+                CalcObject answer = CALC.SYM_EVAL(CALC.ADD.createFunction(uTimesV, CALC.MULTIPLY.createFunction(vduIntegrator.integrate(vTimesDu, var), CALC.NEG_ONE)));
+                if (!containsError(answer)) {
+                    overlord.answer = answer;
+                }
+                //System.out.println("I am " + Thread.currentThread().getName() + " and i found an answer");
             } catch (Exception e) {
                 e.printStackTrace(System.err);
+                System.exit(1);
             }
+        } else {
+            //System.out.println("I gave up; " + toString());
         }
     }
-}
 
+    public boolean containsError(CalcObject test) {
+        return test.toString().contains("Error");
+    }
+}
