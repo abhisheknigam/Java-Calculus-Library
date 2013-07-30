6623bb5d3ed4311608f4b49319c64f9eec8d2254
Seva.Luchianov <Seva.Luchianov@gmail.com>
2013-07-30 17:23:40 +0000

    Makes more sense this way

--- src/javacalculus/evaluator/CalcDEPTH.java
+++ src/javacalculus/evaluator/CalcDEPTH.java
@@ -13,8 +13,8 @@ import javacalculus.struct.CalcObject;
 import javacalculus.struct.CalcSymbol;
 
 /**
- *shows the depth of the nested functions
- * 
+ * shows the depth of the nested functions
+ *
  * @author Seva
  */
 public class CalcDEPTH implements CalcFunctionEvaluator {
@@ -24,31 +24,25 @@ public class CalcDEPTH implements CalcFunctionEvaluator {
         if (input.size() == 1) {
             CalcObject obj = input.get(0);
             return findDepth(obj, new CalcInteger(1));
-        } else if (input.size() == 2) {
-            if (input.get(1) instanceof CalcInteger) {
-                return findDepth(input.get(0), ((CalcInteger) input.get(1)));
-            } else {
-                throw new CalcWrongParametersException("DEPTH -> wrong number of parameters");
-            }
         } else {
             throw new CalcWrongParametersException("DEPTH -> wrong number of parameters");
         }
     }
 
     public CalcInteger findDepth(CalcObject obj, CalcInteger currDepth) {
-        if (obj instanceof CalcFunction) { //input f(x..xn)
-            obj = CALC.SYM_EVAL(obj); //evaluate the function before attempting integration
+        if (obj instanceof CalcFunction) {
+            obj = CALC.SYM_EVAL(obj);
         }
         if (obj.isNumber() || (obj instanceof CalcSymbol)) {
             return currDepth;
         } else if (obj instanceof CalcFunction) {
             CalcFunction objFunc = ((CalcFunction) obj);
             ArrayList<CalcObject> allParts = objFunc.getAll();
-            //currDepth = (CalcInteger) CALC.SYM_EVAL(CALC.ADD.createFunction(currDepth, CALC.ONE));
             CalcInteger testDepth;
             int maxDepth = 0;
             for (CalcObject temp : allParts) {
-                testDepth = (CalcInteger) CALC.SYM_EVAL(CALC.DEPTH.createFunction(temp, currDepth));
+                CalcDEPTH depthFinder = new CalcDEPTH();
+                testDepth = (CalcInteger) CALC.SYM_EVAL(depthFinder.findDepth(temp, currDepth));
                 if (testDepth.intValue() > maxDepth) {
                     maxDepth = testDepth.intValue();
                 }
