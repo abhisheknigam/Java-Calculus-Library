72e240ae01e149422a4f7171049a461f5f9fd486
Seva.Luchianov <Seva.Luchianov@gmail.com>
2013-08-21 16:58:42 +0000

    added global variable

--- src/javacalculus/core/CALC.java
+++ src/javacalculus/core/CALC.java
@@ -30,6 +30,7 @@ public final class CALC {
     public static MathContext mathcontext = MathContext.DECIMAL32;
     public static boolean operator_notation = false;
     public static int max_recursion_depth = 3;
+    public static boolean full_integrate_mode = true;
     /**
      * Standard unaryChar operator symbols
      */
