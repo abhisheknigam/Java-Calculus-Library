4bab1f63fe02e95ceef9e80470f91ace8f0defe2
Seva.Luchianov <Seva.Luchianov@gmail.com>
2013-08-30 18:43:32 +0000

    Pretty significant changes to how this works. Fixed lots of bugs too

--- src/javacalculus/evaluator/CalcSIMPLIFY.java
+++ src/javacalculus/evaluator/CalcSIMPLIFY.java
@@ -27,78 +27,87 @@ public class CalcSIMPLIFY implements CalcFunctionEvaluator {
 
     public CalcObject simplify(CalcObject object) {
         object = CALC.SYM_EVAL(object);
-        //System.out.println("CHECKING: " + object);
+        //System.out.println("SIMPLIFYING: " + object);
         if (object instanceof CalcFunction) {
             ArrayList<CalcObject> multiplyParts = giveList(CALC.MULTIPLY, object);
             CalcObject numeObj = CALC.ONE;
             CalcObject denomObj = CALC.ONE;
             for (CalcObject piece : multiplyParts) {
-                if (piece instanceof CalcFunction && ((CalcFunction) piece).getHeader().equals(CALC.POWER) && ((CalcFunction) piece).get(1).equals(CALC.NEG_ONE)) {
-                    denomObj = CALC.MULTIPLY.createFunction(denomObj, ((CalcFunction) piece).get(0));
+                if (piece instanceof CalcFunction && ((CalcFunction) piece).getHeader().equals(CALC.POWER) && ((CalcFunction) piece).get(1).compareTo(CALC.ZERO) < 0) {
+                    denomObj = CALC.MULTIPLY.createFunction(denomObj, CALC.POWER.createFunction(((CalcFunction) piece).get(0), CALC.ABS.createFunction(((CalcFunction) piece).get(1))));
                 } else {
                     numeObj = CALC.MULTIPLY.createFunction(numeObj, piece);
                 }
             }
             denomObj = CALC.SYM_EVAL(denomObj);
             numeObj = CALC.SYM_EVAL(numeObj);
+            //FACTORING CODE? yeah.
+            //so this might be bad... lets see
+            //System.out.println("DENOM:" + denomObj);
+            //System.out.println("NUME:" + numeObj);
+            denomObj = CALC.SYM_EVAL(CALC.FACTOR.createFunction(denomObj));
+            numeObj = CALC.SYM_EVAL(CALC.FACTOR.createFunction(numeObj));
+            //System.out.println("DENOM FACTORED:" + denomObj);
+            //System.out.println("NUME FACTORED:" + numeObj);
             ArrayList<CalcObject> nume = giveList(CALC.MULTIPLY, numeObj);
             ArrayList<CalcObject> denom = giveList(CALC.MULTIPLY, denomObj);
-            //System.out.println(nume + "///");
-            //System.out.println("///" + denom);
+            ////System.out.println(nume + "///");
+            ////System.out.println("///" + denom);
             ArrayList<CalcObject> process = new ArrayList<>();
-            boolean foundSomething = false;
             for (int i = 0; i < nume.size(); i++) {
-                CalcObject piece = nume.get(i);
-                int index = containsPiece(denom, piece);
-                if (index != -1) {
-                    foundSomething = true;
-                    process.add(CALC.SYM_EVAL(CALC.MULTIPLY.createFunction(piece, CALC.POWER.createFunction(denom.get(index), CALC.NEG_ONE))));
-                    denom.remove(index);
-                } else {
-                    process.add(piece);
+                CalcObject numerator = nume.get(i);
+                for (int j = 0; j < denom.size(); j++) {
+                    CalcObject denominator = denom.get(j);
+                    //System.out.println("TESTING: " + numerator + " / " + denominator);
+                    int initDepth = findTotalDepth(numerator) + findTotalDepth(denominator);
+                    CalcObject toAdd = CALC.SYM_EVAL(CALC.MULTIPLY.createFunction(numerator, CALC.POWER.createFunction(denominator, CALC.NEG_ONE)));
+                    int resultDepth = findTotalDepth(toAdd);
+                    //System.out.println("INIT DEPTH: " + initDepth + " TOADD: " + toAdd + " with depth " + resultDepth);
+                    if (resultDepth < initDepth) {
+                        //System.out.println("ADDING:" + toAdd);
+                        process.add(toAdd);
+                        //System.out.println("PROCESS: "+process);
+                        //System.out.println("OLD NUME: "+nume);
+                        //System.out.println("OLD DENOM: "+denom);
+                        nume.remove(i);
+                        i--;
+                        denom.remove(j);
+                        j=denom.size();
+                        //System.out.println("NEW NUME: "+nume);
+                        //System.out.println("NEW DENOM: "+denom);
+                    }
                 }
-                nume.remove(i);
-                i--;
             }
+            //System.out.println("PROCESS:" + process);
             for (CalcObject piece : denom) {
                 process.add(CALC.POWER.createFunction(piece, CALC.NEG_ONE));
             }
-            if (foundSomething) {
-                CalcObject result = CALC.ONE;
-                for (CalcObject piece : process) {
-                    result = CALC.MULTIPLY.createFunction(result, piece);
-                }
-                return CALC.SYM_EVAL(result);
-            } else {
-                return CALC.MULTIPLY.createFunction(numeObj, CALC.POWER.createFunction(denomObj, CALC.NEG_ONE));
+            for (CalcObject piece : nume) {
+                process.add(piece);
             }
+            //System.out.println("MORE PROCESS:" + process);
+            CalcObject result = CALC.ONE;
+            for (CalcObject piece : process) {
+                result = CALC.SYM_EVAL(CALC.MULTIPLY.createFunction(result, piece));
+            }
+            return result;
         } else {
             return object;
         }
     }
 
-    private int containsPiece(ArrayList<CalcObject> denom, CalcObject piece) {
-        CalcObject testPiece;
-        for (int i = 0; i < denom.size(); i++) {
-            testPiece = denom.get(i);
-            if (testPiece.equals(piece)) {
-                return i;
-            }
-            if (testPiece instanceof CalcFunction) {
-                CalcFunction temp = (CalcFunction) testPiece;
-                if (temp.getHeader().equals(CALC.POWER)) {
-                    if (temp.get(1).isNumber() && temp.get(0).equals(piece)) {
-                        return i;
-                    }
-                }
-            }
+    private int findTotalDepth(CalcObject test) {
+        ArrayList<CalcObject> allParts = giveList(test.getHeader(), test);
+        int totalDepth = 0;
+        for (CalcObject piece : allParts) {
+            totalDepth += ((CalcInteger) CALC.SYM_EVAL(CALC.DEPTH.createFunction(piece))).intValue();
         }
-        return -1;
+        return totalDepth;
     }
 
     private ArrayList<CalcObject> giveList(CalcSymbol operator, CalcObject func) {
         ArrayList<CalcObject> list = new ArrayList<>();
-        //System.out.println(func);
+        ////System.out.println(func);
         if (func instanceof CalcFunction && func.getHeader().equals(operator)) {
             ArrayList<CalcObject> funcParts = ((CalcFunction) func).getAll();
             for (int i = 0; i < funcParts.size(); i++) {
@@ -107,10 +116,10 @@ public class CalcSIMPLIFY implements CalcFunctionEvaluator {
                 list.addAll(giveList(operator, firstObj));
                 //}
             }
-            //System.out.println("LIST in loop" + list);
+            ////System.out.println("LIST in loop" + list);
         } else {
             list.add(func);
-            //System.out.println("LIST" + list);
+            ////System.out.println("LIST" + list);
         }
         return list;
     }
