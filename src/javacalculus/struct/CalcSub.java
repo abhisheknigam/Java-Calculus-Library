4d20d2755582d3171c4de5c922c93be8782db9a8
Seva.Luchianov <Seva.Luchianov@gmail.com>
2013-10-31 16:52:44 +0000

    A u-sub class

---
+++ src/javacalculus/struct/CalcSub.java
@@ -0,0 +1,56 @@
+package javacalculus.struct;
+
+import javacalculus.core.CALC;
+
+public class CalcSub implements CalcObject {
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof CalcError) {
+            return true;
+        }
+        return false;
+    }
+    
+    @Override
+    public boolean isNumber() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public CalcSymbol getHeader() {
+        return CALC.USUB;
+    }
+
+    @Override
+    public CalcObject evaluate() {
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "USub";
+    }
+
+    @Override
+    public int compareTo(CalcObject obj) {
+        return -1;
+    }
+
+    @Override
+    public int getHierarchy() {
+        return CalcObject.SYMBOL;
+    }
+
+    @Override
+    public int getPrecedence() {
+        return -1;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        return hash;
+    }
+}
